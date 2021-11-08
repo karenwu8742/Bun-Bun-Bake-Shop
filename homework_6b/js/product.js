5d15e9806a51700a17918fb8f341bfb34f591fad
Karen Wu <karenwu@andrew.cmu.edu>
2021-11-07 20:18:23 -0500

    Homework 6b

--- homework_6b/js/product.js
+++ homework_6b/js/product.js
@@ -1,7 +1,7 @@
 // Product Javascript
 
 // Creating rolls that are in the cart
-function roll(name, glaze, img, qty, price, cartNum) {
+function roll(name, glaze, img, qty, price, cartNum){
     item = {
         name,
         glaze,
@@ -29,25 +29,25 @@ function roll(name, glaze, img, qty, price, cartNum) {
 }
 
 // Update the Page ordering to user selection
-function vanillaGlaze() {
+function vanillaGlaze(){
     localStorage.setItem('currGlaze', JSON.stringify('vanilla'))
     let currPic = document.getElementById('originalImg');
     currPic.src = "images/original.jpg";
 }
 
-function chocoGlaze() {
+function chocoGlaze(){
     localStorage.setItem('currGlaze', JSON.stringify('chocolate'))
     let currPic = document.getElementById('originalImg');
     currPic.src = "images/chocolate glaze.png";
 }
 
-function sugarMilkGlaze() {
+function sugarMilkGlaze(){
     localStorage.setItem('currGlaze', JSON.stringify('sugar'))
     let currPic = document.getElementById('originalImg');
     currPic.src = "images/gluten.png";
 }
 
-function noGlaze() {
+function noGlaze(){
     localStorage.setItem('currGlaze', JSON.stringify('none'))
     let currPic = document.getElementById('originalImg');
     currPic.src = "images/no glaze.png";
@@ -55,16 +55,30 @@ function noGlaze() {
 
 
 // Update the price on product page
-function updatePrice() {
+function updatePrice(){
     let currPrice = document.getElementById('currPrice');
     let qty = document.getElementById('qtyNum').value;
     let price = (5) * parseInt(qty);
     currPrice.innerText = 'Total: $' + parseFloat(price);
     localStorage.setItem('currPrice', JSON.stringify(parseFloat(price)))
+    updateTotal()
+}
+
+// Update total price on cart page
+function updateTotal(){
+    let price = JSON.parse(localStorage.getItem('currPrice')) //STORE THE CURRENT ITEM'S PRICE
+    let cartPrice = JSON.parse(localStorage.getItem('priceSum')) //IF THERE IS A TOTAL PRICE STORED
+    if (cartPrice){
+        localStorage.setItem('priceSum', JSON.stringify(parseFloat(price) + parseFloat(cartPrice)))
+    }
+    else{
+        localStorage.setItem('priceSum', JSON.stringify(parseFloat(price)))
+    }
+    displayCart()
 }
 
 // Update the cart on the navigaation abr
-function updateCart() {
+function updateCart(){
     let cartNumStored = JSON.parse(localStorage.getItem('cartNum'));
     let selectedQty = parseInt(document.getElementById('qtyNum').value);
     let cartNum = document.getElementById('cart').text;
@@ -78,10 +92,11 @@ function updateCart() {
         localStorage.setItem('cartNum',JSON.stringify('CART (' + selectedQty + ')'));
         document.getElementById('cart').text = 'CART (' + selectedQty + ')'; 
     }
+    displayCart()
 }
 
 // Display the number of rolls in the cart
-function cart() {
+function cart(){
     let cartNumStored = JSON.parse(localStorage.getItem('cartNum'));
     if (cartNumStored) {
         let currCart = document.getElementById('cart');
@@ -91,11 +106,46 @@ function cart() {
     if (priceSum == null) {
         localStorage.setItem('priceSum', JSON.stringify(0))
     }
-    updateCart();
+    updateCart()
+}
+
+// Remove item from cart
+function removeItem(){ 
+    let removeButton = document.getElementsByClassName("changesR"); //getting the remove buttons from cart page
+    for (let t = 0; t < removeButton.length; t++){ 
+        let b = removeButton[t];
+        b.addEventListener('click', function(event) {
+        removeFromTotal();
+        event.target.parentElement.remove();
+        })
+    }   
+}
+
+// After removing item, the total price will also change
+function removeFromTotal(){
+    let cartItems = document.getElementsByClassName("items")[0]; //getting items in cart 
+    let itemRows = cartItems.getElementsByClassName("item"); //each item's specific row 
+    let total = document.getElementById("total").innerText;
+    total = total.replace("Total: $", '');
+    total = parseFloat(total);
+    let priceSum = JSON.parse(localStorage.getItem('priceSum'))
+    if (priceSum ===null){
+        localStorage.setItem('priceSum', JSON.stringify(0))
+    }
+    localStorage.setItem('priceSum', JSON.stringify(total));
+    //console.log(itemRows);
+    for (let i = 0; i < itemRows; i++){
+        //console.log('entered');
+        let item = itemRows[i];
+        let itemPrice = item.getElementsByClassName("price").text;
+        total = total - itemPrice;
+    }
+    document.getElementById("total").text = total;
+    localStorage.setItem('priceSum', JSON.stringify(total));
 }
 
 // Add the number of rolls to the cart
-function addToCart() {
+function addToCart(){
     let name = "Roll with ";
     let glaze = JSON.parse(localStorage.getItem('currGlaze'))
     let img = document.getElementById('originalImg').src;
@@ -109,8 +159,33 @@ function addToCart() {
     price = parseFloat(price)
     roll(name, glaze, img, qty, price, cartNum)
     updateCart()
+    displayCart()
 }
 
+
+// Item will show up in cart after you have added it
 function displayCart() {
-
+    let cartItems = JSON.parse(localStorage.getItem('inCart')) || [];
+    let items = document.getElementById('items')
+    if (cartItems && items){
+        console.log("entered")
+        items.innerHTML  = '';
+        Object.values(cartItems).map(item => {
+                let newItem = 
+                `<div class = "item"> 
+                    <img class = "item-img" src = "${item.img}"> 
+                    <h5 class="item-roll"> ${item.name } ${item.glaze}</h5>
+                    <h5 class="item-price"> $${item.price }.00 </h5>
+                    <h5 class="item-quantity">${item.qty } </h5>
+                    <button class="changesM"> MODIFY </button>
+                    <button class="changesR" ${onclick=removeItem()}> REMOVE </button>
+                </div>
+                `
+               items.innerHTML += newItem
+               console.log(items.innerHTML)
+        })
+        let total = '<div class = "totalPrice">  ${item.price }.00 </div> '        
+    }
 }
+
+displayCart();
